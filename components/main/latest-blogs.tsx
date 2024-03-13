fb9927c1dabb4f7bd644000400fde71b216d9e68
Umesh Thapa <umeshthapa539@gmail.com>
2024-03-13 13:56:59 +0545

    update

--- components/main/latest-blogs.tsx
+++ components/main/latest-blogs.tsx
@@ -6,7 +6,7 @@ export const LatestBlogs = () => {
 <section>
   <div className="py-8 px-20">
       <div className="mx-auto max-w-screen-sm text-center lg:mb-16 mb-8">
-          <h2 className="mb-4 text-3xl lg:text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">Our Blog</h2>
+          <h2 className="mb-4 text-3xl lg:text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">Blog</h2>
           <p className="font-light text-gray-500 sm:text-xl dark:text-gray-400">We use an agile approach to test assumptions and connect with the needs of your audience early and often.</p>
       </div> 
       <div className="grid gap-8 lg:grid-cols-2">
@@ -14,7 +14,7 @@ export const LatestBlogs = () => {
               <div className="flex justify-between items-center mb-5 text-gray-500">
                   <span className="bg-primary-100 text-primary-800 text-xs font-medium inline-flex items-center px-2.5 py-0.5 rounded dark:bg-primary-200 dark:text-primary-800">
                       <svg className="mr-1 w-3 h-3" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M2 6a2 2 0 012-2h6a2 2 0 012 2v8a2 2 0 01-2 2H4a2 2 0 01-2-2V6zM14.553 7.106A1 1 0 0014 8v4a1 1 0 00.553.894l2 1A1 1 0 0018 13V7a1 1 0 00-1.447-.894l-2 1z"></path></svg>
-                      Tutorial
+                      Article
                   </span>
                   <span className="text-sm">14 days ago</span>
               </div>
@@ -24,7 +24,7 @@ export const LatestBlogs = () => {
                   <div className="flex items-center space-x-4">
                       <img className="w-7 h-7 rounded-full" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/jese-leos.png" alt="Jese Leos avatar" />
                       <span className="font-medium dark:text-white">
-                          Jese Leos
+                          Umesh Thapa
                       </span>
                   </div>
                   <a href="#" className="inline-flex items-center font-medium text-primary-600 dark:text-primary-500 hover:underline">
@@ -47,7 +47,7 @@ export const LatestBlogs = () => {
                   <div className="flex items-center space-x-4">
                       <img className="w-7 h-7 rounded-full" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/bonnie-green.png" alt="Bonnie Green avatar" />
                       <span className="font-medium dark:text-white">
-                          Bonnie Green
+                          Umesh Thapa
                       </span>
                   </div>
                   <a href="#" className="inline-flex items-center font-medium text-primary-600 dark:text-primary-500 hover:underline">
