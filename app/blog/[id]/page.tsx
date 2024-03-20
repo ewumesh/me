b136334104f9b0e76c25809e1189b6283ebb33a8
Umesh Thapa <umeshthapa539@gmail.com>
2024-03-20 19:47:22 +0545

    update

--- app/blog/[id]/page.tsx
+++ app/blog/[id]/page.tsx
@@ -2,8 +2,8 @@ export default function ViewBlog() {
     return (
         <section>
             <main className="pt-8 pb-16 lg:pt-16 lg:pb-24 bg-white dark:bg-gray-900 antialiased">
-                <div className="flex justify-between px-4 mx-auto max-w-screen-xl ">
-                    <article className="mx-auto w-full max-w-2xl format format-sm sm:format-base lg:format-lg format-blue dark:format-invert">
+                <div className="flex justify-between px-20 ">
+                    <article className="mx-auto w-full  format format-sm sm:format-base lg:format-lg format-blue dark:format-invert">
                         <header className="mb-4 lg:mb-6 not-format">
                             <address className="flex items-center mb-6 not-italic">
                                 <div className="inline-flex items-center mr-3 text-sm text-gray-900 dark:text-white">
