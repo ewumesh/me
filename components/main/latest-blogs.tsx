d133fbfc2f625f0de8d531fef0db9e635005f9f2
Umesh Thapa <umeshthapa539@gmail.com>
2024-03-24 10:19:59 +0545

    update

--- components/main/latest-blogs.tsx
+++ components/main/latest-blogs.tsx
@@ -1,8 +1,8 @@
-import axios from "axios";
-var latestBlogs: any[] = [];
-var feedDetails: any;
-import { formatDistance, subDays } from 'date-fns';
+'use client'
 
+import React, { useState, useEffect } from 'react';
+import axios from "axios";
+import { formatDistance, subDays } from 'date-fns';
 
 function getTimeDifference(dateString: string) {
     let a = formatDistance(subDays(dateString, 3), new Date(), { addSuffix: true })
@@ -16,16 +16,6 @@ function convertToSlug(inputString: string): string {
     const slugString = lowercaseString.replace(/\s+/g, '-');
     return slugString;
 }
-
-const fetchLatestBlogs = async () => {
-    try {
-        const res = await axios.get('https://api.rss2json.com/v1/api.json?rss_url=https://medium.com/feed/@ewumesh');
-        return res.data; // Return the data fetched from the RSS feed
-    } catch (error) {
-        console.error("Error fetching latest blogs:", error);
-        throw error; // Rethrow the error to handle it in the calling code
-    }
-};
 
 function extractTextFromHTML(html: string): string {
     // Regular expression to match text within paragraph tags
@@ -46,125 +36,61 @@ function extractTextFromHTML(html: string): string {
     return extractedText.join('\n').slice(0, 200) + '...';
 }
 
-
 export const LatestBlogs = () => {
+    const [latestBlogs, setLatestBlogs] = useState<any[]>([]);
+    const [feedDetails, setFeedDetails] = useState<any>({});
 
-    
-    fetchLatestBlogs()
-        .then(data => {
-            latestBlogs = data.items;
-            feedDetails = data.feed;
-            console.log(data.feed, '-----------------------------')
-        })
-        .catch(error => {
-            console.error("Error:", error);
-        });
+    useEffect(() => {
+        const fetchLatestBlogs = async () => {
+            try {
+                const res = await axios.get('https://api.rss2json.com/v1/api.json?rss_url=https://medium.com/feed/@ewumesh');
+                setLatestBlogs(res.data.items);
+                setFeedDetails(res.data.feed);
+            } catch (error) {
+                console.error("Error fetching latest blogs:", error);
+            }
+        };
+
+        fetchLatestBlogs();
+    }, []);
 
     return (
-
         <section>
-            <div className="py-8 px-20">
-                <div className="mx-auto max-w-screen-sm text-center lg:mb-16 mb-8">
-                    <h2 className="mb-4 text-3xl lg:text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">Blog</h2>
-                    <p className="font-light text-gray-500 sm:text-xl dark:text-gray-400">We use an agile approach to test assumptions and connect with the needs of your audience early and often.</p>
-                </div>
-                <div className="grid gap-8 lg:grid-cols-2">
-                    {latestBlogs.map((blog) => (
-                        <article key={blog.guid} className="p-6 bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
-                            <div className="flex justify-between items-center mb-5 text-gray-500">
-                                <span className="bg-primary-100 text-primary-800 text-xs font-medium inline-flex items-center px-2.5 py-0.5 rounded dark:bg-primary-200 dark:text-primary-800">
-                                    <svg className="mr-1 w-3 h-3" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M2 5a2 2 0 012-2h8a2 2 0 012 2v10a2 2 0 002 2H4a2 2 0 01-2-2V5zm3 1h6v4H5V6zm6 6H5v2h6v-2z" clipRule="evenodd"></path><path d="M15 7h1a2 2 0 012 2v5.5a1.5 1.5 0 01-3 0V7z"></path></svg>
-                                    Article
-                                </span>
-                                <span className="text-sm">{getTimeDifference(blog.pubDate)}</span>
-                            </div>
-                            <h2 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"><a href={/blog/ + convertToSlug(blog.title)}>{blog.title}</a></h2>
-                            <p className="mb-5 font-light text-gray-500 dark:text-gray-400">{extractTextFromHTML(blog.description)}</p>
-                            <div className="flex justify-between items-center">
-                                <div className="flex items-center space-x-4">
-                                    <img className="w-7 h-7 rounded-full" src={feedDetails.image} alt={blog.author} />
-                                    <span className="font-medium dark:text-white">
-                                        {blog.author}
+            {latestBlogs.length > 0 && (
+                <div className="py-8 px-20">
+                    <div className="mx-auto max-w-screen-sm text-center lg:mb-16 mb-8">
+                        <h2 className="mb-4 text-3xl lg:text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">Blog</h2>
+                        <p className="font-light text-gray-500 sm:text-xl dark:text-gray-400">We use an agile approach to test assumptions and connect with the needs of your audience early and often.</p>
+                    </div>
+                    <div className="grid gap-8 lg:grid-cols-2">
+                        {latestBlogs.map((blog, index) => (
+                            <article key={index} className="p-6 bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
+                                <div className="flex justify-between items-center mb-5 text-gray-500">
+                                    <span className="bg-primary-100 text-primary-800 text-xs font-medium inline-flex items-center px-2.5 py-0.5 rounded dark:bg-primary-200 dark:text-primary-800">
+                                        <svg className="mr-1 w-3 h-3" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M2 5a2 2 0 012-2h8a2 2 0 012 2v10a2 2 0 002 2H4a2 2 0 01-2-2V5zm3 1h6v4H5V6zm6 6H5v2h6v-2z" clipRule="evenodd"></path><path d="M15 7h1a2 2 0 012 2v5.5a1.5 1.5 0 01-3 0V7z"></path></svg>
+                                        Article
                                     </span>
+                                    <span className="text-sm">{getTimeDifference(blog.pubDate)}</span>
                                 </div>
-                                <a href="#" className="inline-flex items-center font-medium text-primary-600 dark:text-white hover:underline">
-                                    Read more
-                                    <svg className="ml-2 w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
-                                </a>
-                            </div>
-                        </article>
-                    ))}
-                    {/* <article className="p-6 bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
-              <div className="flex justify-between items-center mb-5 text-gray-500">
-                  <span className="bg-primary-100 text-primary-800 text-xs font-medium inline-flex items-center px-2.5 py-0.5 rounded dark:bg-primary-200 dark:text-primary-800">
-                      <svg className="mr-1 w-3 h-3" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M2 5a2 2 0 012-2h8a2 2 0 012 2v10a2 2 0 002 2H4a2 2 0 01-2-2V5zm3 1h6v4H5V6zm6 6H5v2h6v-2z" clipRule="evenodd"></path><path d="M15 7h1a2 2 0 012 2v5.5a1.5 1.5 0 01-3 0V7z"></path></svg>
-                      Article
-                  </span>
-                  <span className="text-sm">14 days ago</span>
-              </div>
-              <h2 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"><a href="#">Our first project with React</a></h2>
-              <p className="mb-5 font-light text-gray-500 dark:text-gray-400">Static websites are now used to bootstrap lots of websites and are becoming the basis for a variety of tools that even influence both web designers and developers influence both web designers and developers.</p>
-              <div className="flex justify-between items-center">
-                  <div className="flex items-center space-x-4">
-                      <img className="w-7 h-7 rounded-full" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/bonnie-green.png" alt="Bonnie Green avatar" />
-                      <span className="font-medium dark:text-white">
-                          Umesh Thapa
-                      </span>
-                  </div>
-                  <a href="#" className="inline-flex items-center font-medium text-primary-600 dark:text-primary-500 hover:underline">
-                      Read more
-                      <svg className="ml-2 w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
-                  </a>
-              </div>
-          </article>    
-          <article className="p-6 bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
-              <div className="flex justify-between items-center mb-5 text-gray-500">
-                  <span className="bg-primary-100 text-primary-800 text-xs font-medium inline-flex items-center px-2.5 py-0.5 rounded dark:bg-primary-200 dark:text-primary-800">
-                      <svg className="mr-1 w-3 h-3" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M2 6a2 2 0 012-2h6a2 2 0 012 2v8a2 2 0 01-2 2H4a2 2 0 01-2-2V6zM14.553 7.106A1 1 0 0014 8v4a1 1 0 00.553.894l2 1A1 1 0 0018 13V7a1 1 0 00-1.447-.894l-2 1z"></path></svg>
-                      Article
-                  </span>
-                  <span className="text-sm">14 days ago</span>
-              </div>
-              <h2 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"><a href="#">How to quickly deploy a static website</a></h2>
-              <p className="mb-5 font-light text-gray-500 dark:text-gray-400">Static websites are now used to bootstrap lots of websites and are becoming the basis for a variety of tools that even influence both web designers and developers influence both web designers and developers.</p>
-              <div className="flex justify-between items-center">
-                  <div className="flex items-center space-x-4">
-                      <img className="w-7 h-7 rounded-full" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/jese-leos.png" alt="Jese Leos avatar" />
-                      <span className="font-medium dark:text-white">
-                          Umesh Thapa
-                      </span>
-                  </div>
-                  <a href="#" className="inline-flex items-center font-medium text-primary-600 dark:text-primary-500 hover:underline">
-                      Read more
-                      <svg className="ml-2 w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
-                  </a>
-              </div>
-          </article> 
-          <article className="p-6 bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
-              <div className="flex justify-between items-center mb-5 text-gray-500">
-                  <span className="bg-primary-100 text-primary-800 text-xs font-medium inline-flex items-center px-2.5 py-0.5 rounded dark:bg-primary-200 dark:text-primary-800">
-                      <svg className="mr-1 w-3 h-3" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M2 5a2 2 0 012-2h8a2 2 0 012 2v10a2 2 0 002 2H4a2 2 0 01-2-2V5zm3 1h6v4H5V6zm6 6H5v2h6v-2z" clipRule="evenodd"></path><path d="M15 7h1a2 2 0 012 2v5.5a1.5 1.5 0 01-3 0V7z"></path></svg>
-                      Article
-                  </span>
-                  <span className="text-sm">14 days ago</span>
-              </div>
-              <h2 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"><a href="#">Our first project with React</a></h2>
-              <p className="mb-5 font-light text-gray-500 dark:text-gray-400">Static websites are now used to bootstrap lots of websites and are becoming the basis for a variety of tools that even influence both web designers and developers influence both web designers and developers.</p>
-              <div className="flex justify-between items-center">
-                  <div className="flex items-center space-x-4">
-                      <img className="w-7 h-7 rounded-full" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/bonnie-green.png" alt="Bonnie Green avatar" />
-                      <span className="font-medium dark:text-white">
-                          Umesh Thapa
-                      </span>
-                  </div>
-                  <a href="#" className="inline-flex items-center font-medium text-primary-600 dark:text-primary-500 hover:underline">
-                      Read more
-                      <svg className="ml-2 w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
-                  </a>
-              </div>
-          </article>                  */}
+                                <h2 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"><a href={'/blog/' + convertToSlug(blog.title)}>{blog.title}</a></h2>
+                                <p className="mb-5 font-light text-gray-500 dark:text-gray-400">{extractTextFromHTML(blog.description)}</p>
+                                <div className="flex justify-between items-center">
+                                    <div className="flex items-center space-x-4">
+                                        <img className="w-7 h-7 rounded-full" src={feedDetails.image} alt={blog.author} />
+                                        <span className="font-medium dark:text-white">
+                                            {blog.author}
+                                        </span>
+                                    </div>
+                                    <a href="#" className="inline-flex items-center font-medium text-primary-600 dark:text-white hover:underline">
+                                        Read more
+                                        <svg className="ml-2 w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
+                                    </a>
+                                </div>
+                            </article>
+                        ))}
+                    </div>
                 </div>
-            </div>
+            )}
         </section>
     )
 }
