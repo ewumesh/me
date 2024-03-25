15757775d9b5286ca7d9a20cf0abb9af70abedac
Umesh Thapa <umeshthapa539@gmail.com>
2024-03-26 05:25:16 +0545

    up

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({ children }: PropsWithChildren) {
         )}
       >
         <Analytics/>
-        {/* <StarsCanvas /> */}
+        <StarsCanvas />
         <Navbar />
         {children}
         <Footer />
