cdbf94b35899a01402bcb26b08b85886b4874d57
Umesh Thapa <umeshthapa539@gmail.com>
2024-04-11 22:58:25 +0545

    update

--- components/main/all-active-blogs.tsx
+++ components/main/all-active-blogs.tsx
@@ -14,6 +14,26 @@ export default function AllActiveBlogs() {
     const [isDeleting, setIsDeleting] = useState(false);
     const [isLoading, setIsLoading] = useState(true);
     const [isOpenPopover, setIsOpenPopover] = useState(false);
+    const [limit, setLimit] = useState<any>(20);
+    const [currentPage, setCurrentPage] = useState(1);
+    const [totalPages, setTotalPages] = useState(1);
+    
+
+    const goToNextPage = () => {
+        if (currentPage < totalPages) {
+            setCurrentPage(currentPage + 1);
+
+            getAllActiveBlogs();
+        }
+    };
+
+    // Function to handle going to the previous page
+    const goToPreviousPage = () => {
+        if (currentPage > 1) {
+            setCurrentPage(currentPage - 1);
+            getAllActiveBlogs();
+        }
+    };
 
     const togglePopover = () => {
         setIsOpenPopover(!isOpenPopover);
@@ -22,13 +42,14 @@ export default function AllActiveBlogs() {
     const getAllActiveBlogs = async () => {
         setIsLoading(true);
         try {
-            const response = await fetch(`${API_URL.url}/api/blog`);
+            const response = await fetch(`${API_URL.url}/api/blog?page=${currentPage}&limit=${limit}`);
             if (!response.ok) {
                 throw new Error(`HTTP error! status: ${response.status}`);
             }
             const data = await response.json();
             setIsLoading(false);
-            setAllBlogs(data);
+            setAllBlogs(data?.blogs);
+            setTotalPages(data.totalBlogs);
         } catch (error) {
             setIsLoading(false);
             console.error("Error fetching blogs:", error);
@@ -85,73 +106,94 @@ export default function AllActiveBlogs() {
         <section>
             <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
                 {!isLoading && (
-                    <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
-                        <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-                            <tr>
-                                <th scope="col" className="px-6 py-3">
-                                    S.N.
-                                </th>
-                                <th scope="col" className="px-6 py-3">
-                                    Title
-                                </th>
-                                <th scope="col" className="px-6 py-3">
-                                    Category
-                                </th>
-                                <th scope="col" className="px-6 py-3">
-                                    Status
-                                </th>
-                                <th scope="col" className="px-6 py-3">
-                                    Published Date
-                                </th>
-
-                                <th scope="col" className="px-6 py-3">
-                                    Action
-                                </th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {allBlogs.map((blog: any, index: number) => (
-                                <tr key={index} className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-                                    <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                                        {index + 1}
+                    <div>
+                        <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                            <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                                <tr>
+                                    <th scope="col" className="px-6 py-3">
+                                        S.N.
                                     </th>
-                                    <td className="px-6 py-4">
-                                        {blog?.title}
-                                    </td>
-                                    <td className="px-6 py-4">
-                                        {blog?.category || '-'}
-                                    </td>
-                                    <td className="px-6 py-4">
-                                        {blog?.status || '-'}
-                                    </td>
-                                    <td>
-                                        {formatDate(blog?.updatedAt) || '-'}
-                                    </td>
-                                    <td className="px-6 py-4">
-                                        <Link href={'/blog-management/edit-blog/'+blog?._id}>
-                                        <button type="button" className="mx-1 text-blue-700 border border-blue-700 hover:bg-blue-700 hover:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-full text-sm p-2.5 text-center inline-flex items-center dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:focus:ring-blue-800 dark:hover:bg-blue-500">
-                                            <svg className="w-[14px] h-[14px] text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
-                                                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m14.304 4.844 2.852 2.852M7 7H4a1 1 0 0 0-1 1v10a1 1 0 0 0 1 1h11a1 1 0 0 0 1-1v-4.5m2.409-9.91a2.017 2.017 0 0 1 0 2.853l-6.844 6.844L8 14l.713-3.565 6.844-6.844a2.015 2.015 0 0 1 2.852 0Z" />
-                                            </svg>
-                                            <span className="sr-only">Icon description</span>
-                                        </button>
-                                        </Link>
+                                    <th scope="col" className="px-6 py-3">
+                                        Title
+                                    </th>
+                                    <th scope="col" className="px-6 py-3">
+                                        Category
+                                    </th>
+                                    <th scope="col" className="px-6 py-3">
+                                        Status
+                                    </th>
+                                    <th scope="col" className="px-6 py-3">
+                                        Published Date
+                                    </th>
 
-                                        <button className="mx-1 text-blue-700 border border-blue-700 hover:bg-blue-700 hover:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-full text-sm p-2.5 text-center inline-flex items-center dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:focus:ring-blue-800 dark:hover:bg-blue-500">
-                                            <svg className="w-[14px] h-[14px] text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
-                                                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 7h14m-9 3v8m4-8v8M10 3h4a1 1 0 0 1 1 1v3H9V4a1 1 0 0 1 1-1ZM6 7h12v13a1 1 0 0 1-1 1H7a1 1 0 0 1-1-1V7Z" />
-                                            </svg>
-
-                                            <span className="sr-only">Icon description</span>
-                                        </button>
-
-                                        
-                                    </td>
-
+                                    <th scope="col" className="px-6 py-3">
+                                        Action
+                                    </th>
                                 </tr>
-                            ))}
-                        </tbody>
-                    </table>
+                            </thead>
+                            <tbody>
+                                {allBlogs.map((blog: any, index: number) => (
+                                    <tr key={index} className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                                        <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                            {index + 1}
+                                        </th>
+                                        <td className="px-6 py-4">
+                                            {blog?.title}
+                                        </td>
+                                        <td className="px-6 py-4">
+                                            {blog?.category || '-'}
+                                        </td>
+                                        <td className="px-6 py-4">
+                                            {blog?.status || '-'}
+                                        </td>
+                                        <td>
+                                            {formatDate(blog?.updatedAt) || '-'}
+                                        </td>
+                                        <td className="px-6 py-4">
+                                            <Link href={'/blog-management/edit-blog/' + blog?._id}>
+                                                <button type="button" className="mx-1 text-blue-700 border border-blue-700 hover:bg-blue-700 hover:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-full text-sm p-2.5 text-center inline-flex items-center dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:focus:ring-blue-800 dark:hover:bg-blue-500">
+                                                    <svg className="w-[14px] h-[14px] text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                                                        <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m14.304 4.844 2.852 2.852M7 7H4a1 1 0 0 0-1 1v10a1 1 0 0 0 1 1h11a1 1 0 0 0 1-1v-4.5m2.409-9.91a2.017 2.017 0 0 1 0 2.853l-6.844 6.844L8 14l.713-3.565 6.844-6.844a2.015 2.015 0 0 1 2.852 0Z" />
+                                                    </svg>
+                                                    <span className="sr-only">Icon description</span>
+                                                </button>
+                                            </Link>
+
+                                            <button className="mx-1 text-blue-700 border border-blue-700 hover:bg-blue-700 hover:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-full text-sm p-2.5 text-center inline-flex items-center dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:focus:ring-blue-800 dark:hover:bg-blue-500">
+                                                <svg className="w-[14px] h-[14px] text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                                                    <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 7h14m-9 3v8m4-8v8M10 3h4a1 1 0 0 1 1 1v3H9V4a1 1 0 0 1 1-1ZM6 7h12v13a1 1 0 0 1-1 1H7a1 1 0 0 1-1-1V7Z" />
+                                                </svg>
+
+                                                <span className="sr-only">Icon description</span>
+                                            </button>
+
+
+                                        </td>
+
+                                    </tr>
+                                ))}
+                            </tbody>
+                        </table>
+
+                        <nav aria-label="Page navigation py-2 my-2">
+            <ul className="inline-flex -space-x-px text-sm">
+                <li>
+                    <button onClick={goToPreviousPage} className="flex items-center justify-center px-3 h-8 ms-0 leading-tight text-gray-500 bg-white border border-e-0 border-gray-300 rounded-s-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">Previous</button>
+                </li>
+                {/* Render page numbers */}
+                {Array.from({ length: totalPages }, (_, i) => i + 1).map(pageNumber => (
+                    <li key={pageNumber}>
+                        <button onClick={() => setCurrentPage(pageNumber)} className={`flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white ${currentPage === pageNumber ? 'text-blue-600 border border-gray-300 bg-blue-50 hover:bg-blue-100 hover:text-blue-700 dark:border-gray-700 dark:bg-gray-700 dark:text-white' : ''}`}>
+                            {pageNumber}
+                        </button>
+                    </li>
+                ))}
+                <li>
+                    <button onClick={goToNextPage} className="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 rounded-e-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">Next</button>
+                </li>
+            </ul>
+        </nav>
+                    </div>
                 )}
 
                 {isLoading && (
