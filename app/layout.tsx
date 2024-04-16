e1b3ca775894d469fabff633fbe4876c91e84404
Umesh Thapa <umeshthapa539@gmail.com>
2024-04-16 12:29:54 +0545

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,6 +30,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: PropsWithChildren) {
   return (
     <html lang="en">
+      <head><meta name="viewport" content="width=device-width, initial-scale=1" /></head>
       <body
         className={cn(
           "bg-[#030014] overflow-y-scroll overflow-x-hidden",
